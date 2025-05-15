c064a98d5bd2ac60ea912ee6785c3b1b6210d1ae
DidierCamargo <didier.camargoc@gmail.com>
2025-05-15 01:28:21 -0600

    Segundo commit

--- app/src/main/java/com/coding/swissarmy/MainActivity.kt
+++ app/src/main/java/com/coding/swissarmy/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.coding.swissarmy
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
@@ -8,6 +9,7 @@ import com.coding.swissarmy.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(){
 
+    private val TAG = MainActivity::class.java.simpleName
     private val binding: ActivityMainBinding by lazy { ActivityMainBinding.inflate(layoutInflater) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,6 +20,10 @@ class MainActivity : AppCompatActivity(){
 
         val navController = navHostFragment.navController
         binding.bottomNav.setupWithNavController(navController)
+    }
+
+    private fun functionGit(){
+        Log.d(TAG, "Function Git")
     }
 
 }
